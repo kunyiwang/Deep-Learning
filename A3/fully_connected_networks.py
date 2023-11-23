0e452201380463d777c9a19abc0a75f6cf9c648a
wangkunyi <kunyiwang252750@gmail.com>
2023-11-23 15:37:08 +0300

    A3/fully_connected_net/Adam

--- A3/fully_connected_networks.py
+++ A3/fully_connected_networks.py
@@ -639,7 +639,12 @@ def adam(w, dw, config=None):
     # NOTE: In order to match the reference output, please modify t _before_ #
     # using it in any calculations.                                          #
     ##########################################################################
-    
+    config['t']+=1
+    config['m'] = config['beta1']*config['m'] + (1-config['beta1'])*dw
+    mt = config['m']/(1-(config['beta1']**config['t']))
+    config['v'] = config['beta2']*config['v'] + (1-config['beta2'])*(dw**2)
+    vt = config['v']/(1-(config['beta2']**config['t']))
+    next_w = w - mt * config['learning_rate']/(torch.sqrt(vt)+config['epsilon'])
     #########################################################################
     #                              END OF YOUR CODE                         #
     #########################################################################
